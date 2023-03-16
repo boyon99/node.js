4867f1e1cbd91035fa070a2d9b43f22882b8b896
boyon99 <bo_yon_99@naver.com>
2023-03-16 17:02:01 +0900

    :memo: / 예외처리

--- main.js
+++ main.js
@@ -6,31 +6,58 @@ let app = http.createServer(function (req, res) {
   let _url = req.url;
   let queryData = url.parse(_url, true).query;
   let pathname = url.parse(_url, true).pathname;
-  let title = queryData.id
 
   if (pathname === '/') {
-    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-      let template = /* HTML */`
-  <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      ${description}
-    </body>
-    </html>
-      `
-      res.end(template)
-    })
+    if (queryData.id === undefined) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err) {
+        let title = "Welcome"
+        let description = "Hello, Node.js"
+        let template = /* HTML */`
+    <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        ${description}
+      </body>
+      </html>
+        `
+        res.end(template)
+      })
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        let title = queryData.id
+        let template = /* HTML */`
+    <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        ${description}
+      </body>
+      </html>
+        `
+        res.end(template)
+      })
+    }
   } else {
     res.writeHead(200)
     res.end('not found')
