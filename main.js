1fc6b68dacbbddb2bb5fef6523dadf23ef6e4247
boyon99 <bo_yon_99@naver.com>
2023-03-16 16:48:50 +0900

    :memo: readFile()를 통해 데이터 읽기

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("main.js")
-
 let http = require('http')
 let fs = require('fs')
 let url = require('url');
@@ -7,14 +5,37 @@ let url = require('url');
 let app = http.createServer(function (req, res) {
   let _url = req.url;
   let queryData = url.parse(_url, true).query;
+  let title = queryData.id
   if (_url == '/') {
-    _url = '/index.html'
+    title = 'Welcome'
   }
   if (req.url == '/favicon.ico') {
     return res.writeHead(404)
   }
   res.writeHead(200)
-  res.end(queryData.id)
+  fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+    let template = `
+    <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    ${description}
+  </body>
+  </html>
+    `
+    res.end(template)
+  })
+
 })
 
 app.listen(1000)
