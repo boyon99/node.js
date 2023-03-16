6197a70336deb33a513cb043a0b90801e4e23ed6
boyon99 <bo_yon_99@naver.com>
2023-03-16 16:54:40 +0900

    :memo: not found 기능 추가

--- main.js
+++ main.js
@@ -5,36 +5,37 @@ let url = require('url');
 let app = http.createServer(function (req, res) {
   let _url = req.url;
   let queryData = url.parse(_url, true).query;
+  let pathname = url.parse(_url, true).pathname;
   let title = queryData.id
-  if (_url == '/') {
-    title = 'Welcome'
+
+  if (pathname === '/') {
+    fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+      let template = /* HTML */`
+  <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      ${description}
+    </body>
+    </html>
+      `
+      res.end(template)
+    })
+  } else {
+    res.writeHead(200)
+    res.end('not found')
   }
-  if (req.url == '/favicon.ico') {
-    return res.writeHead(404)
-  }
-  res.writeHead(200)
-  fs.readFile(`data/${title}`, 'utf8', function (err, description) {
-    let template = `
-    <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    ${description}
-  </body>
-  </html>
-    `
-    res.end(template)
-  })
+
 
 })
 
