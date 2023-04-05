1e148af64768a8fb022f9ea4be42a88e75bde822
boyon99 <bo_yon_99@naver.com>
2023-04-05 17:01:41 +0900

    :memo: Create README.md

--- README.md
+++ README.md
@@ -1,44 +1,11 @@
 # node.js
 
-## PM2
+Node.js는 Chrome V8 JavaScript 엔진으로 빌드 된 JavaScript 런타임이다. 따라서 노드를 통해 다양한 자바스크립트 애플리케이션을 실행할 수 있으며, 서버를 실행하는 데 제일 많이 사용된다.
 
-PM2는 Node.js 어플리케이션을 쉽게 관리할 수 있게 해주는 Process Manager이다.
-
-> PM2 is a daemon process manager that will help you manage and keep your application online. Getting started with PM2 is straightforward, it is offered as a simple and intuitive CLI, installable via NPM.
-
-### 설치하기
+#### 실행하기
 
 ```console
-npm i pm2 -g
+node [파밀명]
 ```
 
-### PM2 명령어
-
-#### 시작하기
-
-```console
-pm2 start <파일명> [--watch]
-```
-
-start를 통해 앱을 실행시킨다. `—watch`를 통해 현재 디렉토리와 그 하위 폴더의 파일을 실시간으로 감시하고 파일을 수정하면 서버를 재실행한다. 수동으로 서버를 재실행하지 않고, 새로고침만으로 확인할 수 있다.
-
-#### 종료하기
-
-```console
-pm2 stop <파일명>
-pm2 delete <파일명>
-pm2 kill
-```
-
-`stop`을 통해 프로세스를 중지하고 `delete`를 통해 프로세스를 삭제할 수 있다. `kill`을 통해 pm2를 종료한다.
-
-#### 확인하기
-
-```console
-pm2 log [<파일명>]
-pm2 list
-pm2 monit
-
-```
-
-`log`을 통해 작업한 로그를 실시간으로 확인할 수 있으며 `list`를 통해 관리하고 있는 파일을 확인할 수 있다. `monit`를 통해 메모리/CPU의 사용량을 확인한다.
+입력 후 localhost:[포트번호]에 접속 시 생성된 서버를 열 수 있다.
--- main.js
+++ main.js
@@ -1,118 +1,20 @@
+console.log("main.js")
+
 let http = require('http')
 let fs = require('fs')
 let url = require('url');
 
-// template HTML
-function templateHTML(title, list, body) {
-  return `
-  <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <a href="/create">create</a>
-      ${body}
-    </body>
-    </html>
-      `
-}
-
-// template list
-function templateList(filelist) {
-  // 반복문을 통해 list에 저장
-  let list = '<ul>'
-  for (let i = 0; i < filelist.length; i++) {
-    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-  }
-  list += "</ul>"
-  return list
-}
-
 let app = http.createServer(function (req, res) {
   let _url = req.url;
   let queryData = url.parse(_url, true).query;
-  let pathname = url.parse(_url, true).pathname;
-
-  // 정상적으로 로드된 경우
-  if (pathname === '/') {
-    // 루트 경로인 경우
-    if (queryData.id === undefined) {
-      // 데이터 폴더 안에 있는 파일이름을 배열 형식으로 가져오기
-      fs.readdir('./data', function (err, filelist) {
-        let list = templateList(filelist)
-        // title
-        let title = "Welcome"
-        let description = "Hello, Node.js"
-        let template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-        res.end(template)
-      })
-
-    } else {
-      // 파일 안에 데이터를 description란 이름으로 저장
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-        // 데이터 폴더 안에 있는 파일이름을 배열 형식으로 가져오기
-        fs.readdir('./data', function (err, filelist) {
-          let list = templateList(filelist)
-          // title
-          let title = queryData.id
-          let template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-          res.end(template)
-        })
-      })
-    }
-  } else if (pathname === '/create') {
-    if (queryData.id === undefined) {
-      // 데이터 폴더 안에 있는 파일이름을 배열 형식으로 가져오기
-      fs.readdir('./data', function (err, filelist) {
-        let list = templateList(filelist)
-        // title
-        let title = "WEB - create"
-        let description = "Hello, Node.js"
-        let template = templateHTML(title, list, `
-          <form action="http://localhost:1000/create_process" method="post">
-          <p>
-            <input type="text" name="title" placeholder="title"></input>
-          </p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit"></input>
-          </p>         
-        </form>
-        `);
-        res.end(template)
-      })
-    }
+  if (_url == '/') {
+    _url = '/index.html'
   }
-  // create_process인 경우
-  else if (pathname === '/create_process') {
-    var body = '';
-    // post로 전송한 데이터 받기
-    req.on('data', function (data) {
-      body += data;
-    })
-
-    req.on('end', function () {
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        res.writeHead(200)
-        console.log(err)
-        res.end('success')
-      })
-    })
+  if (req.url == '/favicon.ico') {
+    return res.writeHead(404)
   }
-  // 그 외에 경로인 경우
-  else {
-    res.writeHead(200)
-    res.end('not found')
-  }
+  res.writeHead(200)
+  res.end(queryData.id)
 })
 
 app.listen(1000)
